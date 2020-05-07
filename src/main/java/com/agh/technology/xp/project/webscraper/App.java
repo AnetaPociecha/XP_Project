ffff0eae8284425d0867812024716bb01d528827
Łukasz <lukasy09@gmail.com>
2020-05-07 19:52:33 +0200

    Application framework setup

---
+++ src/main/java/com/agh/technology/xp/project/webscraper/App.java
@@ -0,0 +1,15 @@
+package com.agh.technology.xp.project.webscraper;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+    }
+}
