b10cf1477818f830f5ba3387277ed77b5cc8cfd4
Łukasz <lukasy09@gmail.com>
2020-05-08 15:09:53 +0200

    removing spring beans

--- src/main/java/com/agh/technology/xp/project/webscraper/Application.java
+++ src/main/java/com/agh/technology/xp/project/webscraper/Application.java
@@ -2,16 +2,12 @@ package com.agh.technology.xp.project.webscraper;
 
 
 import com.agh.technology.xp.project.webscraper.articles.parser.ArticleHeadersParser;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 
 
     public static void main(String[] args) {
-
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-
-        ArticleHeadersParser parser= ctx.getBean("parser", ArticleHeadersParser.class);
+        ApplicationDelegate delegate = new ApplicationDelegate(new ArticleHeadersParser());
+        delegate.testGetArticles();
     }
 }
--- src/main/java/com/agh/technology/xp/project/webscraper/ApplicationDelegate.java
+++ src/main/java/com/agh/technology/xp/project/webscraper/ApplicationDelegate.java
@@ -2,16 +2,18 @@ package com.agh.technology.xp.project.webscraper;
 
 import com.agh.technology.xp.project.webscraper.articles.datamodel.ArticleContainer;
 import com.agh.technology.xp.project.webscraper.articles.parser.ArticleHeadersParser;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
 
 public class ApplicationDelegate {
 
-    @Autowired
     private ArticleHeadersParser parser;
 
-    public void testGetArticles() {
+    ApplicationDelegate(ArticleHeadersParser parser) {
+        this.parser = parser;
+    }
+
+    void testGetArticles() {
         try {
             ArticleContainer container = parser.fetchAndParse("https://www.interia.pl/");
         } catch (IOException e) {
