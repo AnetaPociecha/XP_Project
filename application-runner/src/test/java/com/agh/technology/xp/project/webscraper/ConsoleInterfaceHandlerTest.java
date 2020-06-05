e4b7b2655bd195a0e11098c048f857f652f0c720
Aneta <anpociecha@gmail.com>
2020-06-05 19:41:21 +0200

    test interaction with user

---
+++ application-runner/src/test/java/com/agh/technology/xp/project/webscraper/ConsoleInterfaceHandlerTest.java
@@ -0,0 +1,49 @@
+package com.agh.technology.xp.project.webscraper;
+
+import com.agh.technology.xp.project.webscraper.articles.parser.ArticleHeadersParserImpl;
+import com.agh.technology.xp.project.webscraper.articles.parser.HttpClientImpl;
+import com.agh.technology.xp.project.webscraper.articles.parser.InteriaArticlesListClient;
+import com.agh.technology.xp.project.webscraper.articlescraper.ArticleDetailsClient;
+import com.agh.technology.xp.project.webscraper.articlescraper.HttpClient;
+import com.agh.technology.xp.project.webscraper.articlescraper.InteriaArticleParser;
+import com.agh.technology.xp.project.webscraper.io.CLIPrinter;
+import com.agh.technology.xp.project.webscraper.io.IScanner;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleInterfaceHandlerTest {
+
+    @Test
+    void runCLI() {
+        String resourceUrl = "https://www.interia.pl";
+        ConsoleInterfaceHandler delegate = new ConsoleInterfaceHandler.ConsoleInterfaceHandlerBuilder()
+                .parser(new InteriaArticlesListClient.InteriaArticlesListClientBuilder()
+                        .httpClient(new HttpClientImpl())
+                        .articleHeadersParser(new ArticleHeadersParserImpl(resourceUrl))
+                        .targetUrl(resourceUrl)
+                        .build())
+                .articleDetailsClient(
+                        new ArticleDetailsClient(new HttpClient(), new InteriaArticleParser()))
+                .printer(new CLIPrinter())
+                .scanner(new MockScanner())
+                .build();
+
+        delegate.runCLI();
+    }
+
+    static class MockScanner implements IScanner {
+
+        static int round = 0;
+
+        @Override
+        public Integer scanIntegerFromInput() {
+            return round == 0 ? 1 : 2;
+        }
+
+        @Override
+        public String nextLine() {
+            return round++ == 0 ? "R" : "X";
+        }
+    }
+}
