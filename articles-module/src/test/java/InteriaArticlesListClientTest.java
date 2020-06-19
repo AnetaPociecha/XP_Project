15b23273841014669dae4f46ae9df62100991907
Aneta <anpociecha@gmail.com>
2020-06-19 18:57:23 +0200

    test InteriaArticlesListClient online

---
+++ articles-module/src/test/java/InteriaArticlesListClientTest.java
@@ -0,0 +1,44 @@
+import com.agh.technology.xp.project.webscraper.articles.datamodel.ArticleContainer;
+import com.agh.technology.xp.project.webscraper.articles.parser.ArticleHeadersParserImpl;
+import com.agh.technology.xp.project.webscraper.articles.parser.HttpClientImpl;
+import com.agh.technology.xp.project.webscraper.articles.parser.InteriaArticlesListClient;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InteriaArticlesListClientTest {
+
+    @Test
+    void fetchAndParseValidURL() throws IOException {
+
+        String url = "https://www.interia.pl:443";
+        InteriaArticlesListClient client = new InteriaArticlesListClient.InteriaArticlesListClientBuilder()
+                .httpClient(new HttpClientImpl())
+                .articleHeadersParser(new ArticleHeadersParserImpl(url))
+                .targetUrl(url)
+                .build();
+
+        assertDoesNotThrow(client::fetchAndParse);
+
+        ArticleContainer container = client.fetchAndParse();
+
+        assertNotNull(container.getAllSections());
+        assertNotNull(container.getAllTitles());
+        assertFalse(container.getAllTitles().isEmpty());
+    }
+
+    @Test
+    void fetchAndParseInvalidURL() {
+
+        String url = "https://www.interiaaaa.pl:443";
+        InteriaArticlesListClient client = new InteriaArticlesListClient.InteriaArticlesListClientBuilder()
+                .httpClient(new HttpClientImpl())
+                .articleHeadersParser(new ArticleHeadersParserImpl(url))
+                .targetUrl(url)
+                .build();
+
+        assertThrows(IOException.class, client::fetchAndParse);
+    }
+}
