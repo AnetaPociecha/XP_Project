da6f39fa576f02651e57f1d5fc52abbeb1a85e96
F.Muras <fryderyk.muras@gmail.com>
2020-06-22 17:54:12 +0200

    acceptance tests fix

--- acceptance-tests/src/main/java/com/agh/technology/xp/project/webscraper/ArticleClientAcceptanceTest.java
+++ acceptance-tests/src/main/java/com/agh/technology/xp/project/webscraper/ArticleClientAcceptanceTest.java
@@ -1,6 +1,8 @@
 package com.agh.technology.xp.project.webscraper;
 
 
+import com.agh.technology.xp.project.webscraper.articles.config.ArticleHeadersParserConfig;
+import com.agh.technology.xp.project.webscraper.articles.config.getterstrategy.InvalidGetterStrategyException;
 import com.agh.technology.xp.project.webscraper.articlescraper.*;
 
 public class ArticleClientAcceptanceTest {
@@ -18,8 +20,8 @@ public class ArticleClientAcceptanceTest {
         this.url = url;
     }
 
-    public String title () throws HttpRequestException {
-        IArticleParser parser = new InteriaArticleParser();
+    public String title () throws HttpRequestException, InvalidGetterStrategyException {
+        IArticleParser parser = new InteriaArticleParser(ArticleHeadersParserConfig.defaultConfig());
         ArticleDetailsClient validClient = new ArticleDetailsClient(new HttpClient(), parser);
 
         return validClient.getInteriaArticle(url).getTitle();
--- acceptance-tests/src/main/java/com/agh/technology/xp/project/webscraper/ArticleClientContentAcceptanceTest.java
+++ acceptance-tests/src/main/java/com/agh/technology/xp/project/webscraper/ArticleClientContentAcceptanceTest.java
@@ -1,6 +1,8 @@
 package com.agh.technology.xp.project.webscraper;
 
 
+import com.agh.technology.xp.project.webscraper.articles.config.ArticleHeadersParserConfig;
+import com.agh.technology.xp.project.webscraper.articles.config.getterstrategy.InvalidGetterStrategyException;
 import com.agh.technology.xp.project.webscraper.articlescraper.*;
 
 public class ArticleClientContentAcceptanceTest {
@@ -18,8 +20,8 @@ public class ArticleClientContentAcceptanceTest {
         this.url = url;
     }
 
-    public String content() throws HttpRequestException {
-        IArticleParser parser = new InteriaArticleParser();
+    public String content() throws HttpRequestException, InvalidGetterStrategyException {
+        IArticleParser parser = new InteriaArticleParser(ArticleHeadersParserConfig.defaultConfig());
         ArticleDetailsClient validClient = new ArticleDetailsClient(new HttpClient(), parser);
 
         return validClient.getInteriaArticle(url).getContent();
